b7e213ce4b17734b9be5a161ae2c0c65ba1d1d78
srinivasangopinath <srinivasan.gopinath@gmail.com>
2017-02-27 13:13:49 -0500

    First DB Changes

---
+++ forumdb.py
@@ -0,0 +1,43 @@
+#
+# Database access functions for the web forum.
+# 
+
+import time
+import psycopg2
+## Database connection
+##DB = []
+
+
+
+## Get posts from database.
+def GetAllPosts():
+    '''Get all the posts from the database, sorted with the newest first.
+
+    Returns:
+      A list of dictionaries, where each dictionary has a 'content' key
+      pointing to the post content, and 'time' key pointing to the time
+      it was posted.
+    '''
+	DB = psycopg2.connect("dbname=forum")
+	cur = DB.cursor()
+	cur.execute = ("SELECT time,content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time': str(row[0])} 
+			  for row in cur.fetchall())
+    ##posts.sort(key=lambda row: row['time'], reverse=True)
+	DB.close()
+    return posts
+
+## Add a post to the database.
+def AddPost(content):
+    '''Add a new post to the database.
+
+    Args:
+      content: The text content of the new post.
+    '''
+	DB = psycopg2.connect("dbname=forum")
+	cur = DB.cursor()
+    ##t = time.strftime('%c', time.localtime())
+	cur.execute("INSERT INTO posts (content) VALUES ('%s') " % content)
+    ##DB.append((t, content))
+	DB.commit()
+	DB.close()
